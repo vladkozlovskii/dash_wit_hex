62ed824e2c75a29934620a1776ff58fbae5c642c
Vlad Kozlovskiy <vladkozlovskiy@gmail.com>
2023-08-18 00:43:34 +0300

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+
+# add your project directory to the sys.path
+project_home = u'/home/vladkozlovskiy/dash_hex'
+if project_home not in sys.path:
+ sys.path = [project_home] + sys.path
+
+# need to pass the flask app as "application" for WSGI to work
+# for a dash app, that is at app.server
+# see https://plot.ly/dash/deployment
+from app import app
+application = app.server
+
+
+# In[ ]:
+
+
+
+
